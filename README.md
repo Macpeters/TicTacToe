ced479efccde9537efb56d6981124037ffbd2227
Mac Peters <macpetersdev@gmail.com>
2019-12-29 07:35:18 -0500

    find winning line

--- README.md
+++ README.md
@@ -25,10 +25,8 @@ See the section about [deployment](https://facebook.github.io/create-react-app/d
 
 
 ## TODO: beyond the tutorial
-* Display the location for each move in the format (col, row) in the move history list.
-* Bold the currently selected item in the move list.
 * ~~Rewrite Board to use loops to make the squares instead of hardcoding them.~~
-* Add a toggle button that lets you sort the moves in either ascending or descending order.
+* ~~break up the components so things are more readable (probably requires webpack)~~
 * When someone wins, highlight the three squares that caused the win.
 * When no one wins, display a message about the result being a draw.
-* ~~break up the components so things are more readable (probably requires webpack)~~
+* Display the location for each move in the format (col, row) in the move history list.
--- src/components/game.js
+++ src/components/game.js
@@ -8,6 +8,7 @@ export default class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      winningLine: null,
       stepNumber: 0,
       xIsNext: true,
     }
@@ -19,7 +20,7 @@ export default class Game extends React.Component {
     const squares = current.squares.slice();
 
     // return early and don't handle the click if there is a winner or the square is filled
-    if(this.calculateWinner(squares) || squares[i]) {
+    if(this.calculateWinner(squares, true) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
@@ -40,7 +41,9 @@ export default class Game extends React.Component {
     });
   }
 
-  calculateWinner(squares) {
+  // this method is called handleClick and from render- we won't update the state from
+  // render because that throws an error
+  calculateWinner(squares, updateState) {
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -55,6 +58,11 @@ export default class Game extends React.Component {
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        if(!this.state.winningLine && updateState) {
+          this.setState({
+            winningLine: lines[i],
+          });
+        }
         return squares[a];
       }
     }
@@ -64,8 +72,8 @@ export default class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = this.calculateWinner(current.squares);
-
+    const winner = this.calculateWinner(current.squares, false);
+    
     const moves = history.map((step, move) => {
       const desc = move ? 'Go to move #' + move : 'Go to game start';
       return (
